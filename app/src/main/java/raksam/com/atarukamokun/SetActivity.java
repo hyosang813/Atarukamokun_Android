2c64c794bfb2f31051afa7a355c9c0a877e3a42b
hyosang813 <hyosang813@gmail.com>
2016-01-22 20:43:38 +0900

    セグメントコントロールの配置 styleの変更(Holoで新しいNumberPicker表示)

--- app/src/main/java/raksam/com/atarukamokun/SetActivity.java
+++ app/src/main/java/raksam/com/atarukamokun/SetActivity.java
@@ -4,14 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 public class SetActivity extends Activity {
+
+    //ピッカー配列の生成
+    final int[] pickerArray = {R.id.num3Pick, R.id.num4Pick, R.id.minilotoPick, R.id.loto6Pick, R.id.loto7Pick};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
 
+
+        NumberPicker pick = (NumberPicker)findViewById(R.id.num3_1);
+        pick.setMinValue(0);
+        pick.setMaxValue(20);
+
+
+
+        //「戻る」ボタン
         Button returnButton = (Button)findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,4 +34,9 @@ public class SetActivity extends Activity {
             }
         });
     }
+
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//
+//    }
 }
