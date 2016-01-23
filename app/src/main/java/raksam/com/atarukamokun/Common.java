f9df886c6d078ae255fb9954c771a1160994d877
hyosang813 <hyosang813@gmail.com>
2016-01-23 22:40:31 +0900

    ピッカー群の表示 リセットボタンの機能 commonにピッカー選択値を保持するint[][]を作成してやりとり

--- app/src/main/java/raksam/com/atarukamokun/Common.java
+++ app/src/main/java/raksam/com/atarukamokun/Common.java
@@ -11,12 +11,20 @@ public class Common extends Application {
 
     // グローバルに扱う変数
     Point windowSize; //画面サイズ(x,y)
-    int segChecked;
+    int segChecked; //どのセグメントコントロールがタップされていたかを格納
+    int pickerContents[][] = {
+        {0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0}
+    }; //各ピッカービューの選択値
 
     // 変数を初期化する
     public void init(){
         windowSize = new Point();
         segChecked = R.id.num3SegButton; //初期値はnum3
+
     }
 
 }
--- app/src/main/java/raksam/com/atarukamokun/SetActivity.java
+++ app/src/main/java/raksam/com/atarukamokun/SetActivity.java
@@ -7,23 +7,42 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.RadioGroup;
 
-import java.util.HashMap;
+import android.util.Log;
 
 public class SetActivity extends Activity {
 
     private Common common; // グローバル変数を扱うクラス
 
     //ピッカー配列の生成
-    final int[] pickerArray = {R.id.num3Pick, R.id.num4Pick, R.id.minilotoPick, R.id.loto6Pick, R.id.loto7Pick};
+    private final int[] pickerArray = {R.id.num3Pick, R.id.num4Pick, R.id.minilotoPick, R.id.loto6Pick, R.id.loto7Pick};
 
     //ピッカーの中身の２次元配列
-    final int[][] pickerContentArray = {
+    private final int[][] pickerContentArray = {
             {R.id.num3_1, R.id.num3_2, R.id.num3_3},
             {R.id.num4_1, R.id.num4_2, R.id.num4_3, R.id.num4_4},
             {R.id.mini_1, R.id.mini_2, R.id.mini_3, R.id.mini_4},
             {R.id.loto6_1, R.id.loto6_2, R.id.loto6_3, R.id.loto6_4, R.id.loto6_5},
             {R.id.loto7_1, R.id.loto7_2, R.id.loto7_3, R.id.loto7_4, R.id.loto7_5, R.id.loto7_6}
     };
+
+    //ピッカーに表示する文字列配列
+    private final String[] numbersStrings = new String[] { "ANY", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    private final String[] minilotoStrings = new String[] {"ANY",
+            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+            "31"};
+    private final String[] loto6Strings = new String[] {"ANY",
+            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+            "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+            "41", "42", "43"};
+    private final String[] loto7Strings = new String[] {"ANY",
+            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+            "31", "32", "33", "34", "35", "36", "37"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,40 @@ public class SetActivity extends Activity {
 
         //共通クラス取得
         common = (Common) getApplication();
+
+        //各ピッカーの数列生成と表示
+        for (int i = 0; i < pickerContentArray.length; i++) {
+            for (int j = 0; j < pickerContentArray[i].length; j++) {
+                int maxVal;
+                String[] pickerStrings;
+                switch (i) {
+                    case 0:
+                    case 1:
+                        maxVal = numbersStrings.length - 1;
+                        pickerStrings = numbersStrings;
+                        break;
+                    case 2:
+                        maxVal = minilotoStrings.length - 1;
+                        pickerStrings = minilotoStrings;
+                        break;
+                    case 3:
+                        maxVal = loto6Strings.length - 1;
+                        pickerStrings = loto6Strings;
+                        break;
+                    default:
+                        maxVal = loto7Strings.length - 1;
+                        pickerStrings = loto7Strings;
+                        break;
+                }
+                NumberPicker numberPicker = (NumberPicker)findViewById(pickerContentArray[i][j]);
+                numberPicker.setMinValue(0);
+                numberPicker.setMaxValue(maxVal);
+                numberPicker.setDisplayedValues(pickerStrings);
+                numberPicker.setFocusable(true);
+                numberPicker.setFocusableInTouchMode(true);
+                numberPicker.setValue(common.pickerContents[i][j]);
+            }
+        }
 
         //前回最終で表示されたピッカー群を表示
         View picker = findViewById(radio2picker(common.segChecked));
@@ -68,33 +121,44 @@ public class SetActivity extends Activity {
             @Override
             public void onClick(View v) {
 
+                //各ピッカービューの選択値を共通クラスに格納
+                for (int i = 0; i < pickerContentArray.length; i++) {
+                    for (int j = 0; j < pickerContentArray[i].length; j++) {
+                        NumberPicker numberPicker = (NumberPicker)findViewById(pickerContentArray[i][j]);
+                        common.pickerContents[i][j] = numberPicker.getValue();
+                    }
+                }
+
                 //メイン画面に戻る
                 finish();
+            }
+        });
+
+        //リセットボタン
+        final Button resetButton = (Button)findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //表示されているセグメントコントロールに紐づくピッカー群を全てANYに戻す
+                RadioGroup segGroup = (RadioGroup)findViewById(R.id.pickerSegment);
+                int[] resetPicker = pickerContentArray[segGroup.indexOfChild(findViewById(segGroup.getCheckedRadioButtonId()))];
+                for (int i = 0; i < resetPicker.length; i++) {
+                    NumberPicker numberPicker = (NumberPicker)findViewById(resetPicker[i]);
+                    numberPicker.setValue(0);
+                }
             }
         });
     }
 
     //ラジオボタンと表示するピッカー群を紐付け
     private int radio2picker(int checkedId) {
-        int returnValue;
         switch (checkedId) {
-            case R.id.num3SegButton:
-                returnValue = R.id.num3Pick;
-                break;
-            case R.id.num4SegButton:
-                returnValue = R.id.num4Pick;
-                break;
-            case R.id.minilotoSegButton:
-                returnValue = R.id.minilotoPick;
-                break;
-            case R.id.loto6SegButton:
-                returnValue = R.id.loto6Pick;
-                break;
-            default:
-                returnValue = R.id.loto7Pick;
-                break;
+            case R.id.num3SegButton: return R.id.num3Pick;
+            case R.id.num4SegButton: return R.id.num4Pick;
+            case R.id.minilotoSegButton: return R.id.minilotoPick;
+            case R.id.loto6SegButton: return R.id.loto6Pick;
+            default: return R.id.loto7Pick;
         }
-        return returnValue;
     }
 //    @Override
 //    public void onWindowFocusChanged(boolean hasFocus) {
